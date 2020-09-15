f1a6b2ee71a23224b02e4f9265c374cdcd2b8ac3
retroazure <tomascosta005@outlook.com>
2020-09-15 18:02:52 +0100

    MVP tasks done

--- src/App.js
+++ src/App.js
@@ -1,112 +1,20 @@
-import React, {useState, useEffect} from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
+
 import Form from './components/Form';
-import axios from 'axios';
-import formSchema from './validation/formSchema';
-
-const initialFormValues = {
-  name:'',
-  email:'',
-  password:'',
-  tos: false,
-}
-
-const initialFormErrors = {
-  name:'',
-  email:'',
-  password:'',
-  tos: false,
-}
-
-const initialUser = []; 
-const initialDisabled = true;
-
-
 
 function App() {
 
-  const [users, setUsers] = useState(initialUser);
-  const [formValue, setFormValues] = useState(initialFormValues);
-  const [formErrors, setFormErrors] = useState(initialFormErrors);
-  const [disabled, setDisabled] = useState(initialDisabled);
-
-  const getUsers = () => {
-    axios.get("https://reqres.in/api/users").then((response)=>{
-      console.log(response.data.data);
-      setUsers(response.data.data);
-    }).catch(()=>{
-      console.log("An error has occurred.");
-    })
-  }
-
-  const postUsers = newUser => {
-    axios.post("https://reqres.in/api/users", newUser)
-    .then(res=> {
-      setUsers([...users,res.data.data]);
-      console.log(res.data.data);
-    })
-    .catch(err => {
-      console.log("An error occurred.");
-    })
-    .finally(()=>
-    {
-      setFormValues(initialFormValues);
-    }
-    )
-  }
-
-  const onInput = evt => {
-    const name = evt.target.name;
-    const value = evt.target.value;
-
-    setFormValues({
-      ...formValue,
-      [name]: value
-    })
-  }
-
-  const onCheckBoxChange = evt => {
-    const { name } = evt.target;
-
-    const { checked } = evt.target;
-  }
-
-  const onSubmit = evt => {
-    evt.preventDefault();
-
-    const newUser = {
-      name : formValue.name,
-      email: formValue.email,
-      password: formValue.password,
-      tos: formValue.tos,
-    }
-    
-    postUsers(newUser);
-  }  
-
-  useEffect(()=>{
-    getUsers();
-    
-  }, []);
-
-  useEffect(()=>{
-   formSchema.isValid(formValue)
-   .then(valid => {
-     setDisabled(!valid);
-   })
-  }, [formValue]);
+  const [users, setUsers] = useState([]) //Set on response from axios call - when the new user is created this array will get a new item containing their name
 
   return (
     <div className="App">
-      <Form 
-      values={formValue} 
-      onSubmit={onSubmit} 
-      onInput={onInput}
-      errors={formErrors}
-      disabled={disabled}
-      onCheckBoxChange={onCheckBoxChange}
-      />
+
+      <Form users={users} setUsers={setUsers} />
+
+      {users.map((item, index) => <h3 key={index}>{item}</h3>)}
+
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,69 +1,229 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import * as yup from 'yup';
+import axios from 'axios';
 
-export default function Form(props){
-    const { onInput, onSubmit, values, disabled, errors, onCheckboxChange} = props;
+
+
+let Form = props => {
+
+    let users = props.users;
+    let setUsers = props.setUsers;
+
+    //State 
+
+    const [formState, setFormState] = useState({ //Stores form data here
+    
+        name: '',
+        email: '',
+        password: '',
+        terms: false
+    
+    });
+
+    const [inputErrors, setInputErrors] = useState({ //State for form input validation errors
+        email: "",
+        password: "",
+        terms: ""
+      });
+
+
+      //Input validation schema using Yup
+
+      const formSchema = yup.object().shape({
+        name: yup
+            .string()
+            .min(1, "Type your name")
+            .required("Name is Required"),
+        email: yup
+            .string()
+            .email("Must be a valid email address.")
+            .required("Must include email address."),
+        password: yup
+            .string()
+            .min(6, "Passwords must be at least 6 characters long.")
+            .required("Password is Required"),
+        terms: yup
+            .boolean()
+            .oneOf([true], "You must accept Terms and Conditions")
+      });
+
+
+    //Callback functions
+
+
+    let submitClicked = event => { 
+
+        event.preventDefault()
+
+        axios
+            .post('https://reqres.in/api/users', formState)
+            .then(response => {
+
+                console.log('Data response:', response.data);
+
+                setUsers([
+
+                    ...users,
+                    response.data.name
+
+                ]);
+
+            })
+            .catch(err => {
+
+                console.log(err);
+
+            });
+
+    }
+
+    
+
+    let onFormInput = event => {
+
+        let checked = event.target.parentElement.parentElement.querySelector('.terms').checked;
+
+        //Sets inputErrors state if invalid input
+
+        yup
+            .reach(formSchema, event.target.name)
+            .validate(event.target.value)
+            .then(valid => {
+
+                setInputErrors({
+
+                    ...inputErrors,
+                    [event.target.name]: ''
+
+                });
+
+            })
+            .catch(err => {
+
+                setInputErrors({
+
+                    ...inputErrors,
+                    [event.target.name]: err.errors[0]
+
+                });
+
+            })
+
+        //Sets form state
+
+        setFormState({
+
+            ...formState,
+            [event.target.name]: event.target.value,
+            terms: checked
+            
+        });
+
+        event.persist()
+
+    }
+
+
+    useEffect(() => {
+
+        const formSchema = yup.object().shape({ //Had to copy schema into here for it to work
+            name: yup
+                .string()
+                .min(1, "Type your name")
+                .required("Name is Required"),
+            email: yup
+                .string()
+                .email("Must be a valid email address.")
+                .required("Must include email address."),
+            password: yup
+                .string()
+                .min(6, "Passwords must be at least 6 characters long.")
+                .required("Password is Required"),
+            terms: yup
+                .boolean()
+                .oneOf([true], "You must accept Terms and Conditions")
+          });
+
+        formSchema.isValid(formState).then(valid => { //Enables button if the input is valid
+
+            document.querySelector('form button').disabled = !valid;
+
+        })
+
+    },[formState]) //runs when formState changes
+
 
     return(
-             
-            <form onSubmit={onSubmit}>
 
-                <div className="container">
-            
-                <label>Name: 
-                    <input className="name-input"
-                    type="text"
-                    name = "name"
-                    value={values.name}
-                    maxLength="20"
-                    onChange={onInput}
-                    />        
-                </label>
-           
-    
-       
-                <label>Email: 
-                    <input className="email-input"
-                    type="text"
-                    name = "email"
-                    value={values.email}
-                    maxLength="20"
-                    onChange={onInput}
-                    />        
-                </label>
-    
-                
-           
-                <label>Password: 
-                    <input className="password-input"
-                    type="password"
-                    name = "password"
-                    value={values.password}
-                    onChange={onInput}
-                    />        
-                </label>
-               
-                
-            
-                <label>Terms of Service
-                    <input className="tos-input"
-                    type="checkbox"
-                    name="tos"
-                    value={values.tos}
-                    onChange = {onCheckboxChange}/>
-                </label>
+        <form onSubmit={event => submitClicked(event)}>
 
-                    <button disabled={disabled} onClick={onSubmit}>Submit</button>
-                
-                </div>
+            <label>
 
-                <div className="errors">
-                <div>{errors.name}</div>
-                <div>{errors.email}</div>
-                <div>{errors.password}</div>
-                <div>{errors.tos}</div>
-                </div>
-            
-            </form>
+                Name
+
+                <input 
+                name='name' 
+                type='text' 
+                value={formState.name}
+                onChange={event => onFormInput(event)}>
+                </input>
+
+            </label>
+
+            <label>
+
+                Email
+
+                <input name='email' 
+                type='text' 
+                value={formState.email} 
+                onChange={event => onFormInput(event)}>
+                </input>
+
+            </label>
+
+            <label>
+
+                Password
+
+                <input 
+                name='password' 
+                type='password' 
+                value={formState.password} 
+                onChange={event => onFormInput(event)}>
+                </input>
+
+            </label>
+
+            <label>
+
+                Accept Terms of Service
+
+                <input 
+                className='terms' 
+                name='terms' 
+                type='checkbox' 
+                onChange={event => onFormInput(event)}>
+                </input>
+
+            </label>
+
+            <button disabled={true}>
+                Submit
+            </button>
+
+            <div>
+
+                <h2>{inputErrors.name}</h2>
+                <h2 className='emailerr'>{inputErrors.email}</h2>
+                <h2>{inputErrors.password}</h2>
+
+            </div>
+
+        </form>
 
     )
+
 }
+
+export default Form;
